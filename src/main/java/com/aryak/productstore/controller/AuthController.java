c658b3ccbcefe1e368025ab270ca64be466b5795
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-12 21:40:18 +0530

    JWT configuration added

--- src/main/java/com/aryak/productstore/controller/AuthController.java
+++ src/main/java/com/aryak/productstore/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.aryak.productstore.controller;
 import com.aryak.productstore.model.UserDto;
 import com.aryak.productstore.model.dtos.LoginRequestDto;
 import com.aryak.productstore.model.dtos.LoginResponseDto;
+import com.aryak.productstore.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,21 +24,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody
                                                      LoginRequestDto loginRequestDto) {
         try {
+
             final Authentication authentication = authenticationManager.authenticate(new
                     UsernamePasswordAuthenticationToken(loginRequestDto.username(),
                     loginRequestDto.password()));
+
             final var userDto = new UserDto();
             final var loggedInUser = (User) authentication.getPrincipal();
             userDto.setUsername(loggedInUser.getUsername());
-            //String jwtToken = jwtUtil.generateJwtToken(authentication);
+
+            String jwtToken = jwtUtil.generateJwtToken(authentication);
+
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(),
-                            userDto, "jwtToken"));
+                            userDto, jwtToken));
         } catch (BadCredentialsException ex) {
             return buildErrorResponse(HttpStatus.UNAUTHORIZED,
                     "Invalid username or password");
@@ -45,6 +51,7 @@ public class AuthController {
             return buildErrorResponse(HttpStatus.UNAUTHORIZED,
                     "Authentication failed");
         } catch (Exception ex) {
+            ex.printStackTrace();
             return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,
                     "An unexpected error occurred");
         }
---
+++ src/main/java/com/aryak/productstore/util/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.aryak.productstore.util;
+
+public class ApplicationConstants {
+
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+
+    // MUST have a size >= 256 bits
+    public static final String JWT_SECRET_DEFAULT_VALUE = "dguqywyufdqfwq32242323f252f5578x";
+}
