f37605f0f92d4948359e021201a4a37b3e1e205a
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-12 12:24:15 +0530

    Login controller implemented

--- src/main/java/com/aryak/productstore/controller/AuthController.java
+++ src/main/java/com/aryak/productstore/controller/AuthController.java
@@ -1,6 +1,19 @@
 package com.aryak.productstore.controller;
 
+import com.aryak.productstore.model.UserDto;
+import com.aryak.productstore.model.dtos.LoginRequestDto;
+import com.aryak.productstore.model.dtos.LoginResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +22,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final String test = "test";
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody
+                                                     LoginRequestDto loginRequestDto) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new
+                    UsernamePasswordAuthenticationToken(loginRequestDto.username(),
+                    loginRequestDto.password()));
+            var userDto = new UserDto();
+            var loggedInUser = (User) authentication.getPrincipal();
+            userDto.setUsername(loggedInUser.getUsername());
+            //String jwtToken = jwtUtil.generateJwtToken(authentication);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(),
+                            userDto, "jwtToken"));
+        } catch (BadCredentialsException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED,
+                    "Invalid username or password");
+        } catch (AuthenticationException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED,
+                    "Authentication failed");
+        } catch (Exception ex) {
+            return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "An unexpected error occurred");
+        }
+    }
+
+    private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status, String message) {
+        return ResponseEntity
+                .status(status)
+                .body(new LoginResponseDto(message, null, null));
+    }
+
 
 }
