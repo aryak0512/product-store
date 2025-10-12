b1f405f7aa5ea28ed0ba6c54517a6c0794422ffb
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-12 09:25:31 +0530

    Security config bean added with static credentials

--- src/main/java/com/aryak/productstore/controller/ContactController.java
+++ src/main/java/com/aryak/productstore/controller/ContactController.java
@@ -2,16 +2,22 @@ package com.aryak.productstore.controller;
 
 import com.aryak.productstore.model.ContactRequestDto;
 import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1")
+@RequestMapping(value = "/api/v1/contacts")
 public class ContactController {
 
-    @PostMapping("/contacts")
+    @PostMapping("/save")
     public void saveContact(RequestEntity<ContactRequestDto> contactRequestDtoRequest) {
+        System.out.printf("Received contact request: %s%n", contactRequestDtoRequest.getBody());
+    }
 
+    @GetMapping("/test")
+    public String test() {
+        return "Hey there from contact controller";
     }
 }
